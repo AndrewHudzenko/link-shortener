c0e5ce648a2874d189968591ec4eba6be73286ce
Andrew Hudzenko <s1mpleworlpost@gmail.com>
2024-11-06 16:27:49 +0200

    Initial commit

---
+++ back-end/api-service/src/main/groovy/com/buuufy/apiservice/ApiServiceApplication.groovy
@@ -0,0 +1,13 @@
+package com.buuufy.apiservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ApiServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ApiServiceApplication, args)
+    }
+
+}
