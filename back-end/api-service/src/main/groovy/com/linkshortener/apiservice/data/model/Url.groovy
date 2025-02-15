c50a09900858d94a3341a9d28871c54582739ef2
Andrew Hudzenko <andrewhudzenkowork@gmail.com>
2025-02-15 23:44:33 +0200

    rename UrlMapping.groovy to Url and change internal structure

--- back-end/api-service/src/main/groovy/com/linkshortener/apiservice/data/model/Url.groovy
+++ back-end/api-service/src/main/groovy/com/linkshortener/apiservice/data/model/Url.groovy
@@ -5,24 +5,22 @@ import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.annotation.Version
-import org.springframework.data.mongodb.core.index.IndexDirection
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 
 @ToString
-@Document(collection = "urlMapping")
-class UrlMapping {
+@Document(collection = "Url")
+class Url {
     @Id
     String id
     @CreatedDate
-    @Indexed(background = true, direction = IndexDirection.DESCENDING)
     Date createdDate
     @LastModifiedDate
-    @Indexed(background = true, direction = IndexDirection.DESCENDING)
     Date lastModifiedDate
     @Version
     Long version
     @Indexed(unique = true)
-    String shortLink
-    String originalLink
+    String shortCode
+    String originalUrl
+    int clicks
 }
