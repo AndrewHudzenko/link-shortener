bfcf937305fe6eaaac6797b94d1708fcc6c56138
Andrew Hudzenko <andrewhudzenkowork@gmail.com>
2025-02-15 23:37:57 +0200

    add UrlRequest

---
+++ back-end/api-service/src/main/groovy/com/linkshortener/apiservice/data/model/UrlRequest.groovy
@@ -0,0 +1,10 @@
+package com.linkshortener.apiservice.data.model
+
+import jakarta.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.URL
+
+class UrlRequest {
+    @NotBlank(message = "URL cannot be empty")
+    @URL(message = "Invalid URL format")
+    private String url
+}
