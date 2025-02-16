3c40770156a7d2cc610dbf8e63c20d133da72db2
Andrew Hudzenko <andrewhudzenkowork@gmail.com>
2025-02-16 04:40:10 +0200

    attempt to fix deploy issue

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/link-shortener/front-end/',
+  base: '/link-shortener/',
   build: {
     outDir: 'dist',
   }
