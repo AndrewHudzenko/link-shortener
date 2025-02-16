07c6958aa8df1e4c4c34d9546825886ae2fdf145
Andrew Hudzenko <andrewhudzenkowork@gmail.com>
2025-02-16 02:56:53 +0200

    remove redundant code

--- back-end/api-service/src/main/groovy/com/linkshortener/apiservice/controller/UrlController.groovy
+++ back-end/api-service/src/main/groovy/com/linkshortener/apiservice/controller/UrlController.groovy
@@ -1,11 +1,9 @@
 package com.linkshortener.apiservice.controller
 
-import com.linkshortener.apiservice.annotation.OpenApiCreateShortLink
 import com.linkshortener.apiservice.data.model.Url
 import com.linkshortener.apiservice.data.model.UrlRequest
 import com.linkshortener.apiservice.service.UrlService
 import groovy.util.logging.Slf4j
-import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -15,7 +13,6 @@ import org.springframework.web.servlet.view.RedirectView
 @Slf4j
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
-@Tag(name = "Url Shortener", description = "API for URL shortening")
 class UrlController {
 
     private final UrlService urlService
@@ -24,7 +21,6 @@ class UrlController {
         this.urlService = urlService
     }
 
-//    @OpenApiCreateShortLink
     @PostMapping("/shorten")
     ResponseEntity<Url> shortenUrl(@RequestBody @Valid UrlRequest request) {
         Url url = urlService.createShortUrl(request.url)
